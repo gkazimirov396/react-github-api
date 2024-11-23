3714bb9538f4f3f72ba52a7e8359f4c5a7239e37
gkazimirov396 <grishakazim@gmail.com>
2024-11-23 14:13:44 +0200

    Degug value added

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useDebugValue, useEffect, useState } from 'react';
 
 function useDebounce(value: string, delay = 500) {
-  const [debounced, setDebounced] = useState(value);
+  const [debounced, setDebounced] = useState<string>(value);
 
   useEffect(() => {
     const timerId = setTimeout(setDebounced, delay, value);
 
     return () => clearTimeout(timerId);
   }, [value, delay]);
+
+  useDebugValue(debounced);
 
   return debounced;
 }
